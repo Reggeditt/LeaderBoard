c3280b864f23e29d07c9a9f2f541be7f26f39989
Reggeditt <tkodjosarso@gmail.com>
2023-03-07 13:51:06 +0000

    implement basic interactions and functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './index.css';
+import leaderboardData from './modules/leaderBoardData.js';
+import { submitFormEl, userNameEl, userScoreEl } from './modules/DOMelements.js';
+
+submitFormEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const user = userNameEl.value;
+  const score = userScoreEl.value;
+  leaderboardData.addUserData(user, score);
+  leaderboardData.sortUsersData();
+  leaderboardData.renderUsersData();
+});
